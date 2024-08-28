f4064e50ba9e7a35e39a6f8ab06d5e8fb32d3263
류정우 <evencoding@gmail.com>
2024-08-29 03:25:05 +0900

    remove unused code

--- src/components/icon/svg-components.tsx
+++ src/components/icon/svg-components.tsx
@@ -1,12 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-function generateUUID() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-    const r = (Math.random() * 16) | 0,
-      v = c == 'x' ? r : (r & 0x3) | 0x8
-    return v.toString(16)
-  })
-}
-
 export function ChevronRight({ ...props }) {
   return (
     <svg

18fa153379365b69e58ccb0bfd291a10b1b88d05
류정우 <evencoding@gmail.com>
2024-08-30 19:50:57 +0900

    style: img object-cover

--- src/components/tps.tsx
+++ src/components/tps.tsx
@@ -11,7 +11,7 @@ export const TPS = ({ className }: Props) => {
           <br className="max-md:hidden" /> Parallelized & 10,000 real TPS Chain
         </h2>
         <div className="flex-center mt-[24px] h-[162px] w-full rounded-[13px] bg-black md:mt-[31px] md:h-[293px] md:rounded-[24px]">
-          <img src="/tps.png" alt="tps" className="aspect-[738/293] h-full" />
+          <img src="/tps.png" alt="tps" className="aspect-[738/293] h-full object-cover" />
         </div>
       </div>
     </div>
